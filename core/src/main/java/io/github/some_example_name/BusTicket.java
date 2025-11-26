c743a62a1888cfc51b14d5d0adad4eec106d0286
prz507 <samdsavidge@icloud.com>
2025-11-26 10:21:51 +0000

    test

--- core/src/main/java/io/github/some_example_name/BusTicket.java
+++ core/src/main/java/io/github/some_example_name/BusTicket.java
@@ -5,11 +5,12 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
+
 /**
  * <code> BusTicket </code> represents the ticket used to allow player to board
- * bus and win the game, containing both the actual sprite the user can interact 
+ * bus and win the game, containing both the actual sprite the user can interact
  * with, and UI elements to represent if the ticket has been picked up or not.
- */ 
+ */
 public class BusTicket {
 	private Vector2 position;
 	private Texture texture;
@@ -18,21 +19,21 @@ public class BusTicket {
 
 	/**
 	 * Constructor for <code> BusTicket </code>, spawning the ticket sprite
-	 * in the given world coordinates.Uses "bus-ticket.png" in 
+	 * in the given world coordinates.Uses "bus-ticket.png" in
 	 * <code> assets/ </code> folder for sprite.
-	 * @param x Horizontal position in world for ticket sprite. 
-	 * @param y Vertical position in world for ticket sprite. 
-	 */ 
+	 * @param x Horizontal position in world for ticket sprite.
+	 * @param y Vertical position in world for ticket sprite.
+	 */
 	public BusTicket(float x, float y) {
 		this.position = new Vector2(x, y);
-		this.texture = new Texture("bus-ticket.png"); 
+		this.texture = new Texture("bus-ticket.png");
 		this.isCollected = false;
 		this.isDiscovered = false;
 	}
 
 	/**
 	 * Render ticket sprite within given sprite batch.
-	 * @param batch SpriteBatch to render world sprite in. 
+	 * @param batch SpriteBatch to render world sprite in.
 	 * @see com.badlogic.gdx.graphics.g2d.SpriteBatch SpriteBatch.
 	 */
 	public void render(SpriteBatch batch) {
@@ -42,16 +43,16 @@ public class BusTicket {
 	}
 
 	/**
-	 * Render ticket collected UI element, to signify the ticket is in user 
-	 * inventory. 
-	 * @param batch SpriteBatch to render UI sprite in. 
-	 * @param camera Camera connected to batch. 
+	 * Render ticket collected UI element, to signify the ticket is in user
+	 * inventory.
+	 * @param batch SpriteBatch to render UI sprite in.
+	 * @param camera Camera connected to batch.
 	 * @see com.badlogic.gdx.graphics.g2d.SpriteBatch SpriteBatch.
 	 * @see com.badlogic.gdx.graphics.OrthographicCamera OrthographicCamera
 	 */
 	public void renderAsIcon(SpriteBatch batch, OrthographicCamera camera) {
 		if (isCollected) {
-		    float iconSize = 32f; 
+		    float iconSize = 32f;
 		    float padding = 20f;
 
 		    // Calculate the top-right corner of the camera's view
@@ -68,7 +69,7 @@ public class BusTicket {
 	}
 
 	/**
-	 * Return ticket position as a 2D Vector. 
+	 * Return ticket position as a 2D Vector.
 	 * @return 2D Vector with x/y positions of ticket.
 	 */
 	public Vector2 getPosition() {
@@ -77,7 +78,7 @@ public class BusTicket {
 
 	/**
 	 * Return if the ticket has been collected by player or not.
-	 * @return True/False value corrosponding to if ticket is collected. 
+	 * @return True/False value corrosponding to if ticket is collected.
 	 */
 	public boolean isCollected() {
 		return isCollected;
@@ -98,11 +99,10 @@ public class BusTicket {
 	}
 
 	/**
-	 * Dispose ticket sprite texture.Use as part of 
-	 * Application dispose method. 
+	 * Dispose ticket sprite texture.Use as part of
+	 * Application dispose method.
 	 */
 	public void dispose() {
 		texture.dispose();
 	}
 }
- 
