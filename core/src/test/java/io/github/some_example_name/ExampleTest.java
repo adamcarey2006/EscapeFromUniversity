ce7a22c4dd74c82c32c6a449fe5de204e77e62fb
SSavidge <samdsavidge@icloud.com>
2025-12-20 23:47:23 +0000

    Initial Testing Implementation

---
+++ core/src/test/java/io/github/some_example_name/ExampleTest.java
@@ -0,0 +1,12 @@
+package io.github.some_example_name;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ExampleTest {
+
+    @Test
+    public void test(){
+        assertEquals(2,1+1);
+    }
+}
